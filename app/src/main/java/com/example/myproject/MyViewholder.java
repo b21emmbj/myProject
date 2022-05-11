67eee0c9d68be0bd61cf2fbf3be83da9f334b259
b21emmbj <b21emmbj@student.his.se>
2022-05-11 16:02:02 +0200

    La till en itemholder i layouts + skapade textView i den filen som ska visa upp datan i recyclerviewen, Viewholdern kopplade jag sedan till gränssnittet

--- app/src/main/java/com/example/myproject/MyViewholder.java
+++ app/src/main/java/com/example/myproject/MyViewholder.java
@@ -1,4 +1,24 @@
 package com.example.myproject;
 
-public class MyViewholder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewholder extends RecyclerView.ViewHolder{
+
+    public TextView name;
+    public TextView location;
+    public TextView category;
+    public TextView auxdata;
+
+    public MyViewholder(@NonNull View itemView) {
+        super(itemView);
+        name = itemView.findViewById(R.id.name);
+        location = itemView.findViewById(R.id.location);
+        category = itemView.findViewById(R.id.category);
+        auxdata = itemView.findViewById(R.id.auxdata);
+    }
+
 }
