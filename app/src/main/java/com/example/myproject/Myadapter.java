c0a70c87ef0b7df29ba59fb8dd02fd8464c0065f
b21emmbj <b21emmbj@student.his.se>
2022-05-11 16:12:16 +0200

    kopplade min adapter till min itemholder

--- app/src/main/java/com/example/myproject/Myadapter.java
+++ app/src/main/java/com/example/myproject/Myadapter.java
@@ -1,22 +1,29 @@
 package com.example.myproject;
 
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Myadapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Myadapter extends RecyclerView.Adapter<MyViewholder> {
+
+    private List<Svamp> svamps = new ArrayList();
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewholder((LayoutInflater.from(parent.getContext()).inflate(R.layout.itemholder, parent, false)));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewholder holder, int position) {
 
     }
+
 
     @Override
     public int getItemCount() {
