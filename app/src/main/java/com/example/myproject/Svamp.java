696cdbece2ab669a143fda8536705580130a8ebe
b21emmbj <b21emmbj@student.his.se>
2022-05-11 15:35:28 +0200

    i Svamp.java skapade jag medlemsvariabler och getters & setters för dem

--- app/src/main/java/com/example/myproject/Svamp.java
+++ app/src/main/java/com/example/myproject/Svamp.java
@@ -1,5 +1,40 @@
 package com.example.myproject;
 
 public class Svamp {
+    private String name;
+    private String location;
+    private String category;
+    private String auxdata;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuxdata() {
+        return auxdata;
+    }
+
+    public void setAuxdata(String auxdata) {
+        this.auxdata = auxdata;
+    }
 }
