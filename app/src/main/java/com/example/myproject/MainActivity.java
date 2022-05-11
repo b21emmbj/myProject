d2439eafa232bd85cb39a7abfa33da001692415f
b21emmbj <b21emmbj@student.his.se>
2022-05-11 15:09:10 +0200

    skapade medlemsvariabler och kopplat recyclerview (datatypen) till id;et på min recyclerview och kopplat adaptern till den

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api/getdataasjson.php?type=b21emmbj";
+    private List<Svamp> listofSvampar;
+    private Myadapter adapter;
+    RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listofSvampar = new ArrayList<Svamp>();
+        adapter = new Myadapter();
+
+        recyclerView = findViewById(R.id.myRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
