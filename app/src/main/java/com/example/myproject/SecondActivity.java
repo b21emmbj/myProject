cff24654b100fb1d68dbc8621dfa950fa2d3732d
b21emmbj <b21emmbj@student.his.se>
2022-05-16 16:34:21 +0200

    skapade ett intent i secondactivity för "back/tillbaka"-knappen för att byta skärm (till mainActivity)

--- app/src/main/java/com/example/myproject/SecondActivity.java
+++ app/src/main/java/com/example/myproject/SecondActivity.java
@@ -2,13 +2,25 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        back = findViewById(R.id.back_button);
+        back.setOnClickListener(view -> {
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
+
 }
