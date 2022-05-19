97f464ba85068d698b2286799cb7a2eb0676df49
b21emmbj <b21emmbj@student.his.se>
2022-05-19 13:56:59 +0200

    skapade en hyperlänk till bakgrundsbilden som används i mainActivity. Skapade en funktion (i SecondActivity.java) som gör länken klickbar, launchar web browser och navigerar till länken. Länken ändras i strings.xml

--- app/src/main/java/com/example/myproject/SecondActivity.java
+++ app/src/main/java/com/example/myproject/SecondActivity.java
@@ -3,8 +3,11 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     private Button back;
@@ -18,6 +21,12 @@ public class SecondActivity extends AppCompatActivity {
             Intent intent = new Intent(SecondActivity.this, MainActivity.class);
             startActivity(intent);
         });
+        setupHyperLink();
+    }
+    private void setupHyperLink() {
+        TextView linkTextView = findViewById(R.id.img_source);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        linkTextView.setLinkTextColor(Color.BLUE);
     }
 
 }
